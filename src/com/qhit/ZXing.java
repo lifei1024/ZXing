6fa6c080349ef81be8805417a9ca2a23ca9ff985
李继光 <2327913863@qq.com>
2019-05-14 08:10:42 +0800

    Initial commit

---
+++ src/com/qhit/ZXing.java
@@ -0,0 +1,12 @@
+package com.qhit;
+
+public class ZXing {
+
+	public static void main(String[] args) {
+		
+			int width = 300;//图片的宽度
+			int height = 300;//高度
+			String format = "phg";//图片的格式
+	}
+
+}
